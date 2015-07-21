e9c2694f3517dd36d1e63d2ef02ddf365f046336
weiyi.li <weiyi.just2@gmail.com>
2015-07-22 01:04:32 +0800

    python align-mp3-txt.py mp3file txtfile
    Former-commit-id: acc4e05541e90ea691a10a2cb9df5b6b95641bdd
    Former-commit-id: 3f55686db85a390c39e8b90bfdca03335f985c2a

---
+++ aligner/align-mp3-txt.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+import subprocess
+import os
+from os import path
+import urllib
+import codecs
+import sys
+import shutil
+import json
+import re
+from collections import OrderedDict
+from time import time as clock
+import bookinfo
+
+def buildFilePathWithNewSuffix(file, suffix):
+    dir = path.dirname(file)
+    base = path.basename(file)
+    base = path.splitext(base)[0]
+    return dir + '/' + base + '.' + suffix    
+
+def align(argv):
+    """ Main function for this module (see its docstring for usage) """
+    print sys.argv[0] + " " + sys.argv[1] + " " + sys.argv[2]
+    argv_mp3_file = sys.argv[1]
+    argv_txt_file = sys.argv[2]
+
+    init_start_time = clock()
+    
+    __dir__ = path.realpath(path.dirname(__file__))
+    long_audio_aligner_path = __dir__ + '/long-audio-aligner'
+    mp3_file = __dir__ + '/' + argv_mp3_file
+    txt_file = __dir__ + '/' + argv_txt_file
+    wav_file = buildFilePathWithNewSuffix(mp3_file, 'wav')
+    timings_file = buildFilePathWithNewSuffix(txt_file, 'json')
+    
+    # Build the Java project
+    cwd = os.path.realpath(os.curdir)
+    os.chdir(long_audio_aligner_path)
+    print "Running ant"
+    # retcode = subprocess.call(['ant'])
+    # if retcode != 0:
+    #   raise Exception("fail (have you ant?)")
+    os.chdir(cwd)
+  
+    print "Creating WAV"
+    subprocess.call(['sox', mp3_file, wav_file, 'rate', '16k'])
+
+    print "Updating batch"
+    f = open(long_audio_aligner_path + '/resource/batchFile.txt', 'w')
+    f.write(txt_file + ' ' + wav_file)
+    f.close()	
+
+    print "Aligning text"
+
+    cwd = path.realpath(path.curdir)
+    os.chdir(long_audio_aligner_path)
+    retcode = subprocess.call(['java', '-Xmx3g', '-jar', 'bin/aligner.jar'])
+    if retcode != 0:
+        raise Exception("fail (haz Java?)")
+    os.chdir(cwd)
+
+    # Obtain the timed output
+    fi = codecs.open(long_audio_aligner_path + '/timedOutput/1.txt', encoding='utf-8')
+    raw_timings = fi.read().split()
+    fi.close()
+
+    verse_timings = OrderedDict()
+    word_timings = []
+    
+    # Parse the timings out of the raw timings, and then pair up the
+    # normalized word from Sphinx with the actual word from the text
+    normalize_word_chunk = lambda s: re.sub(r'\W', '', s).lower()
+    stip_punc = lambda s: re.sub(r'^\W+|\W+$', '', s)
+    current_verse = None
+    for raw_timing in raw_timings:
+        matches = re.match(r'(.+)\((.+),(.+)\)', raw_timing)
+        word = matches.group(1)
+
+        start = float(matches.group(2))
+        end = float(matches.group(3))
+        
+        # Keep track of verse timings
+        #if verse_timings[current_verse]['start'] is None:
+        #    verse_timings[current_verse]['start'] = start
+        #verse_timings[current_verse]['end'] = end
+        
+        # Record word timings
+        #word_timings.append({
+        #    'word'  : word,
+        #    'start' : start,
+        #    'end'   : end,
+        #})
+        word_timings.append([word, start, end ])
+    
+    fo = codecs.open(timings_file, mode='w', encoding='utf-8')
+    #fo.write(json.dumps({'verses': verse_timings, 'words': word_timings}, indent=2))
+    fo.write(json.dumps({'words': word_timings}, indent=1))
+    fo.close()
+
+    print "Total execution time: %.02fs" % (clock() - init_start_time)
+
+if __name__ == '__main__':
+    try:
+        align(sys.argv[1:])
+    except Exception as e:
+        print "Exception:", e
+        sys.exit(1)
