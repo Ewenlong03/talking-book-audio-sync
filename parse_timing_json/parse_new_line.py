296c2c624370aad62d56dcc294484e5c0ab6aade
weiyi.li <weiyi.just2@gmail.com>
2015-08-20 00:28:03 +0800

    Fix bug: 目前app处理timing-json的方法是：读取字符串创建TextView然后添加到LinearLayout，如果string的宽度超过行宽，则添加到下一个行；但是若字符串长度超过行宽，将导致超出部分不被显示。这个脚本的作用是，拆分字符串！

--- parse_timing_json/parse_new_line.py
+++ parse_timing_json/parse_new_line.py
@@ -22,7 +22,7 @@ for json_word in json_words_array:
     string = json_word[0]
     timing = json_word[1]
     if "\n" in string:
-        print string + str(json_word[1])
+        print string + "  " +str(json_word[1])
         cLastIndex = 0
         for cIndex, c in enumerate(string, 0):
             if c=="\n" or cIndex == len(string)-1:
@@ -46,6 +46,49 @@ for json_word in json_words_array:
     else:
         result_array.append(json_word)
         
+# creat a dictionary
+result_dict = {
+    "words" : result_array
+}
+
+# Create json file
+output_json_file = JSON_FILE_PATH + ".out.json"
+with open(output_json_file, "w") as outfile:
+    json.dump(result_dict, outfile, indent=4)
+
+
+
+# 如果一个字符串包含超过5个字
+
+json_array = load_json_file(output_json_file)
+json_words_array = json_array["words"]
+result_array = []
+
+# json_words_array 是一个二维数组，它的每一个元素：
+# json_word 是一个数组，["\nIf", 2.035]
+for json_word in json_words_array:
+    # 如果包含换行符，则拆分这个字符串，换行符单独处理。
+    string = json_word[0]
+    timing = json_word[1]
+    splited_string_array = string.split(" ")
+    if len(splited_string_array) > 5:
+        print splited_string_array
+        count = 0
+        splited_string = ""
+        for idx, word in enumerate(splited_string_array, 0):
+            splited_string += word + " "
+            count = count + 1
+            if count >= 3 or idx == len(splited_string_array)-1:
+                json_word_temp = []
+                json_word_temp.append(splited_string)
+                json_word_temp.append(timing)
+                result_array.append(json_word_temp)
+                print splited_string
+                count = 0
+                splited_string = ""
+    else:
+        result_array.append(json_word)
+
 # creat a dictionary
 result_dict = {
     "words" : result_array
