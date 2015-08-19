2533a9a6de7b1e54442fd1820925ddc65a8f82b3
weiyi.li <weiyi.just2@gmail.com>
2015-08-19 22:53:53 +0800

    处理换行符：如果json文件中的字符串包含换行符，则以换行符为界拆分字符串，换行符单独拿出来。目的是方便app处理换行。

---
+++ parse_timing_json/parse_new_line.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+import array
+
+# Load json file
+def load_json_file(file_path):
+    with open(file_path) as timing_json_file:
+        return json.load(timing_json_file)
+
+
+JSON_FILE_PATH = sys.argv[1]
+json_array = load_json_file(JSON_FILE_PATH)
+json_words_array = json_array["words"]
+result_array = []
+
+# json_words_array 是一个二维数组，它的每一个元素：
+# json_word 是一个数组，["\nIf", 2.035]
+for json_word in json_words_array:
+    # 如果包含换行符，则拆分这个字符串，换行符单独处理。
+    string = json_word[0]
+    timing = json_word[1]
+    if "\n" in string:
+        print string + str(json_word[1])
+        cLastIndex = 0
+        for cIndex, c in enumerate(string, 0):
+            if c=="\n" or cIndex == len(string)-1:
+                if cIndex == 0 or cIndex == len(string)-1:
+                    json_word_temp = []
+                    json_word_temp.append(string[cLastIndex:cIndex+1])
+                    json_word_temp.append(json_word[1])
+                    result_array.append(json_word_temp)
+                    cLastIndex = cIndex + 1
+                else:
+                    json_word_temp = []
+                    json_word_temp.append(string[cLastIndex:cIndex])
+                    json_word_temp.append(json_word[1])
+                    result_array.append(json_word_temp)
+                    cLastIndex = cIndex
+                    json_word_temp = []
+                    json_word_temp.append(string[cLastIndex:cIndex+1])
+                    json_word_temp.append(json_word[1])
+                    result_array.append(json_word_temp)
+                    cLastIndex = cIndex+1
+    else:
+        result_array.append(json_word)
+        
+# creat a dictionary
+result_dict = {
+    "words" : result_array
+}
+
+# Create json file
+output_json_file = JSON_FILE_PATH + ".out.json"
+with open(output_json_file, "w") as outfile:
+    json.dump(result_dict, outfile, indent=4)
+
